9295ffcee9c5a17afd7ff1512f10fb22784589c7
Masao Kitamura <masaok@gmail.com>
2021-02-25 10:49:58 -0800

    Permission errors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React from 'react'
 
 import Demo from './components/Demo'
 
+// import { ThemeProvider, makeStyles } from '@material-ui/core/styles'
+
+// const useStyles = makeStyles(
+//   theme => ({
+//     app: {
+//       // width: '100vw',
+//       // height: '100vh',
+//       // padding: 0,
+//       // margin: 0,
+//       // backgroundColor: theme.palette.common.white, // switch to black for debugging
+//       // backgroundColor: theme.palette.primary.light, // switch to black for debugging
+//     },
+//   }),
+//   { name: 'App' }
+// )
+
 const App = () => {
   console.log('APP')
 
-  return <Demo />
+  return (
+    // <ThemeProvider theme={theme}>
+    <Demo />
+    // </ThemeProvider>
+  )
 }
 
 export default App
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,6 +5,11 @@ import 'react-activity-feed/dist/index.es.css'
 
 import { getToken } from '../api'
 
+import { connect } from 'getstream'
+// or if you are on commonjs
+// const { connect } = require('getstream');
+// Instantiate new client with a user token
+
 const API_KEY = process.env.REACT_APP_API_KEY
 const APP_ID = process.env.REACT_APP_APP_ID
 
@@ -14,6 +19,7 @@ const Demo = () => {
   console.log(`APP_ID: ${APP_ID}`)
 
   const [token, setToken] = useState('')
+  const [client, setClient] = useState(null)
 
   useEffect(() => {
     console.log('USE EFFECT')
@@ -25,12 +31,44 @@ const Demo = () => {
     retrieveToken()
   }, [])
 
+  useEffect(() => {
+    console.log('TOKEN EFFECT')
+    if (token) {
+      const client = connect(API_KEY, token, APP_ID)
+      setClient(client)
+    }
+  }, [token])
+
+  useEffect(() => {
+    console.log('CLIENT EFFECT')
+    if (client) {
+      // Create the user: https://getstream.io/activity-feeds/docs/javascript/users_create/
+      // Error: "You do not have permission to do this"
+      // Status: 403, code 17, NotAllowedException
+      // console.log('CREATE USER with getOrCreate')
+      client.user('john-doe').getOrCreate({
+        name: 'John Doe',
+        occupation: 'Software Engineer',
+        gender: 'male',
+      })
+
+      // Instantiate a feed object server side
+      console.log('CREATE USER')
+      const user1 = client.feed('user', 'john-doe')
+
+      // // Create a new activity
+      const activity = { actor: 1, verb: 'tweet', object: 1, foreign_id: 'tweet:1' }
+      user1.addActivity(activity)
+    }
+  }, [client])
+
   return (
     <div style={{ width: '600px', margin: '0 auto' }}>
       {token && (
         <StreamApp apiKey={API_KEY} appId={APP_ID} token={token}>
           <StatusUpdateForm />
-          <FlatFeed feedGroup="user" notify />
+          {/* <FlatFeed feedGroup="user" notify /> */}
+          <FlatFeed feedGroup="user" />
         </StreamApp>
       )}
     </div>
